3e02360a2bda487fc02fb61cd9e1d0f063b9af1b
Vishnu K <kvishnu9497@gmail.com>
2018-06-05 09:33:17 +0530

    updated

--- Pick_A_Bite/app/src/main/java/com/example/vishnuk/pick_a_bite/add_address.java
+++ Pick_A_Bite/app/src/main/java/com/example/vishnuk/pick_a_bite/add_address.java
@@ -1,12 +1,17 @@
 package com.example.vishnuk.pick_a_bite;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.vishnuk.pick_a_bite.OrderSummary.Order_Summary;
 
@@ -26,44 +31,38 @@ public class add_address extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // todo: goto back activity from here
-                Intent intent = new Intent(this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
-                return true;
-
-            case R.id.ic_shopping_cart:
-                Intent intent1 = new Intent(this,Order_Summary.class);
-                startActivity(intent1);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.hometoolbar, menu);
-        // Create a condition (hide it if the count < 0)
-        if (mNotificationCounter >= 0) {
+        if (TabbedActivity.mNotificationCounter >= 0 ) {
             BadgeCounters.update(this,
-                    menu.findItem(R.id.cart_badge_in_tabview),
+                    menu.findItem(R.id.ic_shopping_cart),
                     R.drawable.ic_shopping_cart_36dp,
                     BadgeCounters.BadgeColor.BLUE,
-                    mNotificationCounter);
-        } else {
-            BadgeCounters.hide(menu.findItem(R.id.cart_badge_in_tabview));
+                    TabbedActivity.mNotificationCounter);
         }
-
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.ic_shopping_cart) {
+            if (TabbedActivity.mNotificationCounter >= 0 ) {
+                Intent intent = new Intent(this,Order_Summary.class);
+                startActivity(intent);
+            }
+        }
+        if (id == android.R.id.home)
+        {
+            // todo: goto back activity from here
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void perform_action(View view) {
